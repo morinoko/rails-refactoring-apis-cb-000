2ee56b21172ed509df0b0ebeb3cae85f1805efe2
Felice Forby <feliceforby@gmail.com>
2019-03-07 22:44:56 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,12 +1,18 @@
 class RepositoriesController < ApplicationController
+  before_action :set_access_hash
+
   def index
-    access_hash = { 'access_token' => session[:token] }
     github = GithubService.new(access_hash)
     @repos_array = github.get_repos
   end
 
   def create
+    github = GithubService.new(access_hash)
     response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     redirect_to '/'
+  end
+
+  def set_access_hash
+    access_hash = { 'access_token' => session[:token] }
   end
 end
